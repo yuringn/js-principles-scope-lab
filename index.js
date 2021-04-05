aa493e6c9fa526cb565183c8cfb8ede99bb47c75
Yuri Nguyen <yuringuyen312@gmail.com>
2021-04-04 22:43:48 -0400

    commit tree 637db872ab7badab8afef363415952de2d51f2f9

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = "bob"
+const leastFavoriteCustomer = "Thomas"
+
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+return customerName;}
+
+function setBestCustomer(){
+    bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "Eve";
+}
